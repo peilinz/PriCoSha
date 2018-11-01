57c3e8fbbd8ff90b624663d8f2aad166e2b1231f
Peilin Zhen <pzhen1998@hotmail.com>
2018-11-01 14:17:23 -0400

    make changes to for errors

--- PriCoSha_Database.sql
+++ PriCoSha_Database.sql
@@ -1,6 +1,6 @@
--- Person
+-- Creating tables 
 
-CREATE TABLE Person(
+CREATE TABLE IF NOT EXISTS Person(
 	email VARCHAR(50),
 	password CHAR(32),
 	first_name VARCHAR(50),
@@ -8,16 +8,16 @@ CREATE TABLE Person(
 	PRIMARY KEY(email)
 );
 
-CREATE TABLE FriendGroup(
+CREATE TABLE IF NOT EXISTS FriendGroup(
 	fg_name VARCHAR(50),
 	description VARCHAR(50),
 	email VARCHAR(50),
 	PRIMARY KEY (fg_name, email),
-FOREIGN KEY (email) REFERENCES Person(email)
+	FOREIGN KEY (email) REFERENCES Person(email)
 );
 
-CREATE TABLE ContentItem(
-	item_id VARCHAR(50),
+CREATE TABLE IF NOT EXISTS ContentItem(
+	item_id INT,
 	post_time DATETIME,
 	file_path VARCHAR(50),
 	item_name VARCHAR(50),
@@ -25,10 +25,10 @@ CREATE TABLE ContentItem(
 );
 
 
-CREATE TABLE Tag(
+CREATE TABLE IF NOT EXISTS Tag(
 	tagger VARCHAR(50),
 	tagged VARCHAR(50),
-	item_id VARCHAR(50),
+	item_id INT,
 	status VARCHAR(50),
 	tag_time DATETIME,
 	PRIMARY KEY(tagger, tagged, item_id),
@@ -37,9 +37,9 @@ CREATE TABLE Tag(
 	FOREIGN KEY(item_id) REFERENCES ContentItem(item_id)
 );
 
-CREATE TABLE Rate(
+CREATE TABLE IF NOT EXISTS Rate(
 	email VARCHAR(50),
-	item_id VARCHAR(50),
+	item_id INT,
 	rate_time DATETIME,
 	emoji VARCHAR(50),
 	FOREIGN KEY(email) REFERENCES Person(email),
@@ -47,27 +47,27 @@ CREATE TABLE Rate(
 );
 
 
-CREATE TABLE own(
+CREATE TABLE IF NOT EXISTS Own(
 	email VARCHAR(50),
 	fg_name VARCHAR(50),
 	FOREIGN KEY (email) REFERENCES Person(email),
 	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name)
 );
-CREATE TABLE belong(
+CREATE TABLE IF NOT EXISTS Belong(
 	email VARCHAR(50),
 	fg_name VARCHAR(50),
 	FOREIGN KEY (email) REFERENCES Person(email),
 	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name) 
 );
-CREATE TABLE posted(
+CREATE TABLE IF NOT EXISTS Posted(
 	email VARCHAR(50),
-	item_id int,
-	FOREIGN KEY (email) REFERENCES Person(email)
+	item_id INT,
+	FOREIGN KEY (email) REFERENCES Person(email),
 	FOREIGN KEY (item_id) REFERENCES ContentItem(item_id)
 );
-CREATE TABLE share(
+CREATE TABLE IF NOT EXISTS Share(
 	fg_name VARCHAR(50),
-	item_id int,
+	item_id INT,
 	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name),
 	FOREIGN KEY (item_id) REFERENCES ContentItem(item_id)
 );
