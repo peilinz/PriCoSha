e97da20167430f54232c189c951041ad82bb4358
Peilin Zhen <pzhen1998@hotmail.com>
2018-11-01 16:21:43 -0400

    adding command to view

--- PriCoSha_Inserts.sql
+++ PriCoSha_Inserts.sql
@@ -1,28 +1,28 @@
 -- Insert people
 INSERT INTO Person(email, password, first_name, last_name) VALUES
-('AA@nyu.edu', SHA256('AA'), 'Ann', 'Anderson'),
-('BB@nyu.edu', SHA256('BB'), 'Bob', 'Baker'),
-('CC@nyu.edu', SHA256('CC'), 'Cathy', 'Chang'),
-('DD@nyu.edu', SHA256('DD'), 'David','Davidson'),
-('EE@nyu.edu', SHA256('EE'), 'Ellen', 'Ellenberg'),
-('FF@nyu.edu', SHA256('FF'), 'Fred', 'Fox'),
-('GG@nyu.edu', SHA256('GG'), 'Gina', 'Gupta'),
-('HH@nyu.edu', SHA256('HH'), 'Helen', 'Harper');
+('AA@nyu.edu', SHA2('AA',256), 'Ann', 'Anderson'),
+('BB@nyu.edu', SHA2('BB',256), 'Bob', 'Baker'),
+('CC@nyu.edu', SHA2('CC',256), 'Cathy', 'Chang'),
+('DD@nyu.edu', SHA2('DD',256), 'David','Davidson'),
+('EE@nyu.edu', SHA2('EE',256), 'Ellen', 'Ellenberg'),
+('FF@nyu.edu', SHA2('FF',256), 'Fred', 'Fox'),
+('GG@nyu.edu', SHA2('GG',256), 'Gina', 'Gupta'),
+('HH@nyu.edu', SHA2('HH',256), 'Helen', 'Harper');
 
 INSERT INTO FriendGroup(fg_name, description, owner_email) VALUES
 ('family', 'Anns Family', 'AA@nyu.edu'),
 ('roommates', 'Anns Roommates', 'AA@nyu.edu'),
 ('family', 'Bobs Family', 'BB@nyu.edu');
 
-INSERT INTO Own(email, fg_name) VALUES
+INSERT INTO Own(owner_email, fg_name) VALUES
 ('AA@nyu.edu', 'family'),
 ('AA@nyu.edu', 'roommates'),
 ('BB@nyu.edu', 'family');
 
-INSERT INTO Belong(email, fg_name) VALUES
-('AA@nyu.edu', 'roommates'),
-('GG@nyu.edu', 'roommates'),
-('HH@nyu.edu', 'roommates');
+INSERT INTO Belong(member_email, fg_name, creator_email) VALUES
+('AA@nyu.edu', 'roommates','AA@nyu.edu'),
+('GG@nyu.edu', 'roommates','AA@nyu.edu'),
+('HH@nyu.edu', 'roommates','AA@nyu.edu'),
 
 ('AA@nyu.edu', 'family', 'AA@nyu.edu'),
 ('CC@nyu.edu', 'family', 'AA@nyu.edu'),
@@ -30,8 +30,8 @@ INSERT INTO Belong(email, fg_name) VALUES
 ('EE@nyu.edu', 'family', 'AA@nyu.edu'),
 
 ('BB@nyu.edu', 'family', 'BB@nyu.edu'),
-('FF@nyu.edu', 'family', 'AA@nyu.edu'),
-('EE@nyu.edu', 'family', 'AA@nyu.edu');
+('FF@nyu.edu', 'family', 'BB@nyu.edu'),
+('EE@nyu.edu', 'family', 'BB@nyu.edu');
 
 
 INSERT INTO ContentItem(item_id,post_time, item_name, is_pub) VALUES
@@ -39,7 +39,7 @@ INSERT INTO ContentItem(item_id,post_time, item_name, is_pub) VALUES
 (2, CURRENT_TIMESTAMP, 'leftovers in fridge', 0),
 (3, CURRENT_TIMESTAMP, 'Rover', 0);
 
-INSERT INTO Posted(email, item_id) VALUES
+INSERT INTO Posted(poster_email, item_id) VALUES
 ('AA@nyu.edu', 1),
 ('AA@nyu.edu', 2),
 ('BB@nyu.edu', 3);
--- PriCoSha_Query.sql
+++ PriCoSha_Query.sql
@@ -1,3 +1,4 @@
 -- Write a query to show the ID and name of each content item that is shared with David
 
-SELECT item_id, item_name FROM ContentItem WHERE
+SELECT DISTINCT (item_id, item_name) FROM ContentItem WHERE 
+	is_pub = 1 OR item_id IN (SELECT item_id FROM Share NATURAL JOIN FriendGroup NATURAL JOIN Belong WHERE first_name = 'dd@nyu.edu') OR item_id IN (SELECT item_id FROM Tag WHERE tagger = 'dd@nyu.edu' OR tagged = 'dd@nyu.edu');
