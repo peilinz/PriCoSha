5f00a67e2a85b51533926d2d871cea6c7e8c1f0b
Peilin Zhen <pzhen1998@hotmail.com>
2018-11-17 16:17:22 -0500

    adding more attributes to friendgroups

--- PriCoSha_Database.sql
+++ PriCoSha_Database.sql
@@ -17,12 +17,14 @@ CREATE TABLE IF NOT EXISTS FriendGroup(
 );
 
 CREATE TABLE IF NOT EXISTS ContentItem(
-	item_id INT,
+	item_id INT AUTO_INCREMENT,
+	email_post VARCHAR(50),
 	post_time TIMESTAMP,
 	file_path VARCHAR(50),
 	item_name VARCHAR(50),
 	is_pub BOOLEAN,
-	PRIMARY KEY(item_id)
+	PRIMARY KEY(item_id),
+	FOREIGN KEY(email_post) REFERENCES Person(email_post)
 );
 
 
@@ -42,7 +44,7 @@ CREATE TABLE IF NOT EXISTS Rate(
 	rater_email VARCHAR(50),
 	item_id INT,
 	rate_time TIMESTAMP,
-	emoji VARCHAR(50),
+	emoji VARCHAR(50) CHARACTER SET utf8mb4,
 	PRIMARY KEY(rater_email, item_id),
 	FOREIGN KEY(rater_email) REFERENCES Person(email),
 	FOREIGN KEY(item_id) REFERENCES ContentItem(item_id)
@@ -53,31 +55,32 @@ CREATE TABLE IF NOT EXISTS Own(
 	owner_email VARCHAR(50),
 	fg_name VARCHAR(50),
 	PRIMARY KEY (owner_email, fg_name),
-	FOREIGN KEY (owner_email) REFERENCES Person(email),
-	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name)
+	FOREIGN KEY (fg_name, owner_email) REFERENCES FriendGroup(fg_name, owner_email)
 );
+
 CREATE TABLE IF NOT EXISTS Belong(
 	member_email VARCHAR(50),
 	fg_name VARCHAR(50),
 	creator_email VARCHAR(50),
 	PRIMARY KEY (member_email, fg_name, creator_email),
-	FOREIGN KEY (creator_email) REFERENCES Person(email),
 	FOREIGN KEY (member_email) REFERENCES Person(email),
-	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name) 
+	FOREIGN KEY (fg_name,creator_email) REFERENCES FriendGroup(fg_name, creator_email) 
 );
+
 CREATE TABLE IF NOT EXISTS Posted(
 	poster_email VARCHAR(50),
-	item_id INT,
+	item_id INT AUTO_INCREMENT,
 	PRIMARY KEY (poster_email, item_id),
 	FOREIGN KEY (poster_email) REFERENCES Person(email),
 	FOREIGN KEY (item_id) REFERENCES ContentItem(item_id)
 );
+
 CREATE TABLE IF NOT EXISTS Share(
 	fg_name VARCHAR(50),
 	item_id INT,
 	owner_email VARCHAR(50),
 	PRIMARY KEY (fg_name, item_id, owner_email),
-	FOREIGN KEY (fg_name) REFERENCES FriendGroup(fg_name),
+	FOREIGN KEY (fg_name, owner_email) REFERENCES FriendGroup(fg_name, owner_email),
 	FOREIGN KEY (item_id) REFERENCES ContentItem(item_id)
 );
 
--- PriCoSha_Query.sql
+++ PriCoSha_Query.sql
@@ -1,4 +1,6 @@
 -- Write a query to show the ID and name of each content item that is shared with David
 
-SELECT DISTINCT (item_id, item_name) FROM ContentItem WHERE 
-	is_pub = 1 OR item_id IN (SELECT item_id FROM Share NATURAL JOIN FriendGroup NATURAL JOIN Belong WHERE first_name = 'dd@nyu.edu') OR item_id IN (SELECT item_id FROM Tag WHERE tagger = 'dd@nyu.edu' OR tagged = 'dd@nyu.edu');
+SELECT DISTINCT item_id, item_name FROM ContentItem 
+	WHERE is_pub = 1 OR 
+		item_id IN (SELECT item_id FROM Share NATURAL JOIN FriendGroup NATURAL JOIN Belong WHERE member_email = 'dd@nyu.edu') OR 
+		item_id IN (SELECT item_id FROM Tag WHERE tagger = 'dd@nyu.edu' OR tagged = 'dd@nyu.edu') 
