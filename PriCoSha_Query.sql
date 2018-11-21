37dea646681272282214b5810725cb65922e4e7d
Peilin Zhen <pzhen1998@hotmail.com>
2018-11-21 14:41:39 -0500

    fixing query for part 2

--- PriCoSha_Query.sql
+++ PriCoSha_Query.sql
@@ -2,5 +2,5 @@
 
 SELECT DISTINCT item_id, item_name FROM ContentItem 
 	WHERE is_pub = 1 OR 
-		item_id IN (SELECT item_id FROM Share NATURAL JOIN FriendGroup NATURAL JOIN Belong WHERE member_email = 'dd@nyu.edu') OR 
-		item_id IN (SELECT item_id FROM Tag WHERE tagger = 'dd@nyu.edu' OR tagged = 'dd@nyu.edu') 
+		item_id IN (SELECT item_id FROM Share JOIN Belong ON email = creator_email AND share.fg_name = belong.fg_name WHERE member_email = 'dd@nyu.edu') OR 
+		item_id IN (SELECT item_id FROM Tag WHERE tagger = 'dd@nyu.edu' OR tagged = 'dd@nyu.edu')
